d7bb90dbdcaf27156531c698c6ada3a7e39d62b1
ismael gaton gonzalez <ismael gaton gonzalez>
2016-11-22 21:03:41 +0100

    corrigiendo

--- index.php
+++ index.php
@@ -10,33 +10,36 @@
 </head>
 <body>
 <?php
-$messi=$_GET['messi'];
-$ronaldo=$_GET['ronaldo'];
-$griezman=$_GET['griezman'];
+if (isset($_GET['messi']) && isset($_GET['ronaldo'] )&& isset($_GET['griezman'])){
+    $messi=$_GET['messi'];
+    $ronaldo=$_GET['ronaldo'];
+    $griezman=$_GET['griezman'];
+}
+
 if (isset($_GET['error'])){
     $error=$_GET['error'];
     if ($error&8){
-        echo "<p>la suma es menor de 100</p>";
+        echo "<h1>la suma es menor de 100</h1>";
         if ($error&1){
-            echo "<p>messi mal</p>";
+            echo "<h1>messi mal</h1>";
         }
         if ($error&2){
-            echo "<p>ronaldo mal</p>";
+            echo "<h1>ronaldo mal</h1>";
         }
         if ($error&4){
-            echo "<p>griezman mal</p>";
+            echo "<h1>griezman mal</h1>";
         }
     }
     if ($error&16){
-        echo "<p>la suma es mayor de 100</p>";
+        echo "<h1>la suma es mayor de 100</h1>";
         if ($error&1){
-            echo "<p>messi mal</p>";
+            echo "<h1>messi mal</h1>";
         }
         if ($error&2){
-            echo "<p>ronaldo mal</p>";
+            echo "<h1>ronaldo mal</h1>";
         }
         if ($error&4){
-            echo "<p>griezman mal</p>";
+            echo "<h1>griezman mal</h1>";
         }
     }
 }
@@ -46,9 +49,11 @@ if (isset($_GET['error'])){
     <form action="pagina.php" id="formulario">
         <label for="messi">Messi</label>
         <input type="text" id="messi" name="messi" <?php
-        echo "value='";
-        echo $messi;
-        echo "'";
+        if (isset($_GET['messi'])){
+            echo "value='";
+            echo $messi;
+            echo "'";
+        }
         if (isset($_GET['error'])){
             if ($error&1){
                 echo ("style='background-color:red'");
@@ -57,9 +62,11 @@ if (isset($_GET['error'])){
         ?>><br>
         <label for="ronaldo">Cristiano Ronaldo</label>
         <input type="text" id="ronaldo" name="ronaldo" <?php
-        echo "value='";
-        echo $ronaldo;
-        echo "'";
+        if (isset($_GET['ronaldo'])){
+            echo "value='";
+            echo $ronaldo;
+            echo "'";
+        }
         if (isset($_GET['error'])){
             if ($error&2){
                 echo ("style='background-color:red'");
@@ -68,9 +75,11 @@ if (isset($_GET['error'])){
         ?>><br>
         <label for="griezman">Griezmann</label>
         <input type="text" id="griezman" name="griezman" <?php
-        echo "value='";
-        echo $griezman;
-        echo "'";
+        if (isset($_GET['messi'])){
+            echo "value='";
+            echo $griezman;
+            echo "'";
+        }
         if (isset($_GET['error'])){
             if ($error&4){
                 echo ("style='background-color:red'");
